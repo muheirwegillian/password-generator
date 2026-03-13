5a069f5fc5b547ba8729e72135d7fc2802406ce2
Muheirwe Oyebare Gillian <muheirwegillian@gmail.com>
2026-03-13 00:13:47 -0700

    Add password generator options and slider value display functionality

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let inputSlider = document .getElementById("inputSlider")
-let sliderValue = document.getElementById("sliderValue")
+let inputSlider = document .getElementById("inputSlider");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+
+
+
+
+// showing input slider value
+sliderValue.textContent =inputSlider.value;
+inputSlider.addEventListener('input',()=>{
+    sliderValue.textContent = inputSlider.value;
+})
